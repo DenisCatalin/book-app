faeb718cbea6adde5ac36eeec1c4d2eb1094fc16
Denis <deniscotecata@gmail.com>
2025-01-17 22:08:10 +0200

    qstash workflows for sign-up

--- app/api/auth/workflows/onboarding/route.ts
+++ app/api/auth/workflows/onboarding/route.ts
@@ -1,43 +1,69 @@
+import { db } from "@/database/drizzle";
+import { eq } from "drizzle-orm";
+import { users } from "@/database/schema";
 import { serve } from "@upstash/workflow/nextjs";
+import { sendEmail } from "@/lib/workflow";
+
+type UserState = "non-active" | "active";
 
 type InitialData = {
   email: string;
+  fullName: string;
 };
 
+const ONE_DAY_IN_MS = 60 * 60 * 24 * 1000;
+const THREE_DAYS_IN_MS = ONE_DAY_IN_MS * 3;
+const ONE_MONTH_IN_MS = ONE_DAY_IN_MS * 30;
+
 export const { POST } = serve<InitialData>(async context => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
 
   await context.run("new-signup", async () => {
-    await sendEmail("Welcome to the platform", email);
+    await sendEmail({
+      email,
+      subject: "Welcome to the book-app",
+      message: `Welcome ${fullName}`,
+    });
   });
 
-  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
+  await context.sleep("wait-for-3-days", THREE_DAYS_IN_MS);
 
   while (true) {
     const state = await context.run("check-user-activity", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
       await context.run("sent-email-non-active", async () => {
-        await sendEmail("Email to non-active users", email);
+        await sendEmail({
+          email,
+          subject: "Are you still there?",
+          message: `Hey ${fullName}, we miss you!`,
+        });
       });
     } else if (state === "active") {
       await context.run("sent-email-active", async () => {
-        await sendEmail("Send newsletter to active users", email);
+        await sendEmail({
+          email,
+          subject: "Welcome back!",
+          message: `Hey ${fullName}, we missed you!`,
+        });
       });
     }
 
-    await context.sleep("wait-for-1-day", 60 * 60 * 24 * 30);
+    await context.sleep("wait-for-1-month", ONE_MONTH_IN_MS);
   }
 });
 
-async function sendEmail(message: string, email: string) {
-  console.log(`Sending ${message} email to ${email}`);
-}
+const getUserState = async (email: string): Promise<UserState> => {
+  const user = await db.select().from(users).where(eq(users.email, email)).limit(1);
+  if (user.length === 0) return "non-active";
 
-type UserState = "non-active" | "active";
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const timeDifference = now.getTime() - lastActivityDate.getTime();
 
-async function getUserState(): Promise<UserState> {
-  return "non-active";
-}
+  if (timeDifference > THREE_DAYS_IN_MS && timeDifference <= ONE_MONTH_IN_MS) return "non-active";
+
+  return "active";
+};
